155155a25685b4d42724548e0c77b321fc65970a
huanghuamei <510118271@qq.com>
2018-01-23 09:21:25 +0800

    task#js修改

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-console.log('feature-hhm-0000000')
-
-console.log('feature-hhm-01')
-console.log('feature-hhm')
-console.log('test0666')
-console.log('faeture')
-console.log("hhmtest")
+console.log("20180123")
+console.log("今天必须要完成")
